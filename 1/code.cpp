5965e26e6e7a69aecd021d573d1b4e52f3afff08
americast <sayan.sinha@iitkgp.ac.in>
2017-07-27 16:09:48 +0530

    Add comments

--- 1/code.cpp
+++ 1/code.cpp
@@ -1,3 +1,9 @@
+/* Name: code.cpp
+  Creator: Sayan Sinha (sayan.sinha@iitkgp.ac.in)
+  Date: July 27, 2017
+  Description: Finding peak in an unknown range of numbers
+*/
+
 #include <bits/stdc++.h>
 
 extern void registerme ( const char [] );
@@ -5,35 +11,61 @@ extern int evaluate ( unsigned int );
 extern void verify ( unsigned int );
 using namespace std;
 
+/* Name: findpeak()
+  Input: <none>
+  Output: nth index where the peak is present
+  Description: This program uses a divide-and-conquer iteration for
+     computing peak in an unknown range. It estimates the range with
+     expoenents of 2 after which performs gradient ascent towards the
+     peak.
+*/
 int findpeak()
 {
-	unsigned int now, powernow=0, dir=1, res=0, powerlast=0,count=0, prevres, prevnow=0, prevprevnow=0;
+	unsigned int now, powernow=0, res=0, powerlast=0,count=0, prevres, prevnow=0, prevprevnow=0; //temp variables
+																								 // for storing 
 	while(1)
 	{
 		prevres=res;
 		prevprevnow=prevnow;
 		prevnow=now;
-		now=powerlast+(dir)*pow(2,powernow);
+		now=powerlast+pow(2,powernow);
 		res=evaluate(now);
 		if(count!=0){
-		if (prevres>res && dir==1)
-			{cout<<"here\n";cout<<prevres<<"  "<<res<<"  now:"<<now<<endl; dir=-1; powerlast=now; powernow=-1; break;}
+		if (prevres>res)
+			break;
 		}
 		else count++;
 		powernow++;
 	}
-	unsigned int start=prevprevnow, end=now;
+	unsigned int start=prevprevnow, end=now; //perform search
+	int slope=0;
 	while(1)
 	{
 		unsigned int mid=(start+end)/2;
-		res=evaluate(mid);
-		int res_lesshalf=evaluate(mid-1), res_more_half=evaluate(mid+1);
-		cout<<res_lesshalf<<" "<<res_more_half<<" mid: "<<mid<<"\n";
-		cout<<"start: "<<start<<" end: "<<end<<endl<<endl;
-		if (res_lesshalf<res && res_more_half>res)
-			start=mid;
-		else if (res_lesshalf>res && res_more_half<res)
-			end=mid;
+		res=0;
+		int res_lesshalf=evaluate(mid-1), res_more_half=evaluate(mid+1);   //evaluate gradient
+		// cout<<res_lesshalf<<" "<<res_more_half<<" mid: "<<mid<<"\n";
+		// cout<<"start: "<<start<<" end: "<<end<<endl<<endl;
+		if (res_lesshalf<res_more_half)
+		{
+			if (slope==-1)
+			{
+				res=evaluate(mid);
+				if (res>res_lesshalf && res>res_more_half)
+					return mid;
+			}
+			start=mid; slope=1;
+		}
+		else if (res_lesshalf>res_more_half)
+		{
+			if (slope==1)
+			{
+				res=evaluate(mid);
+				if (res>res_lesshalf && res>res_more_half)
+					return mid;
+			}
+			end=mid; slope=-1;
+		}
 		else return mid;
 	}
 }
