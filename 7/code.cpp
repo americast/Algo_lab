5853b94f740f4704056803ef0a8b050c9697c6a3
americast <sayan.sinha@iitkgp.ac.in>
2017-09-07 14:48:37 +0530

    Week7 print

---
+++ 7/code.cpp
@@ -0,0 +1,81 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct tree
+{
+	tree *parent;
+	int weight;
+};
+
+void printlabyrinth(int **H, int **V,int m,int n)
+{
+	for (int i=1;i<=n;i++)
+		cout<<"+---";
+	cout<<"+\n";
+	int m_here=0;
+	while(1)
+	{
+		cout<<"|   ";
+		for (int i=0;i<n-1;i++)
+			if (V[m_here][i])
+				cout<<"|   ";
+			else
+				cout<<"    ";
+		cout<<"|\n";
+
+		if (m_here < m-1)
+		{
+			for (int i=0;i<n;i++)
+			{
+				cout<<"+";
+				if (H[m_here][i])
+					cout<<"---";
+				else
+					cout<<"   ";
+			}
+			cout<<"+\n";
+		}
+		if(++m_here == m)
+			break;
+	}
+	for (int i=1;i<=n;i++)
+		cout<<"+---";
+	cout<<"+\n";
+}
+
+int main()
+{
+	int m,n;
+	cin>>m>>n;
+	tree* arr[m][n];
+	for (int i=0;i<m;i++)
+		for (int j=0;j<n;j++)
+		{
+			tree *T = (tree*)malloc(sizeof(tree));
+			T -> parent = T;
+			T -> weight = 1;
+			arr[i][j]=T;
+		}
+
+	int **H,**V;
+	H = new int *[m-1];
+	for(int i = 0; i <m-1; i++)
+	    H[i] = new int[n];
+
+	V = new int *[m];
+	for(int i = 0; i <m; i++)
+	    V[i] = new int[n-1];
+
+	// int H[m-1][n], V[m][n-1];
+
+	for (int i=0;i<m-1;i++)
+		for (int j=0;j<n;j++)
+			H[i][j]=1;
+
+	for (int i=0;i<m;i++)
+		for (int j=0;j<n-1;j++)
+			V[i][j]=1;
+
+	printlabyrinth(H,V,m,n);
+
+}
