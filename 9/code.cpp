ad9a5cba81f3bbbaa5a4d510ed0ca6ce637b1583
americast <sayan.sinha@iitkgp.ac.in>
2017-10-12 15:13:42 +0530

    Week9 Part3

--- 9/code.cpp
+++ 9/code.cpp
@@ -11,7 +11,7 @@ void generate_gpa(int arr[], int n)
 	float Pr[] = {0.05,0.2,0.3,0.15};
 	for (int i=1;i<4; i++)
 		Pr[i] += Pr[i-1];
-	for (int i=1;i<=n;i++)
+	for (int i=0;i<n;i++)
 	{
 		float y = (float)rand()/RAND_MAX;
 		// Decision tree begins
@@ -32,6 +32,19 @@ void generate_gpa(int arr[], int n)
 	}
 }
 
+int* countingsort1(int cg[], int n)
+{
+	int C[501]={0}, *B;
+	B = (int *) malloc(n*sizeof(int));
+	for (int i=0; i<n; i++)
+		C[cg[i]]++;
+	for (int i=1; i<501; i++)
+		C[i] += C[i-1];
+	for (int i=0;i<n;i++)
+		B[(C[cg[i]]--)-1] = cg[i];
+	return B;
+}
+
 int main()
 {
 	srand(time(NULL));
@@ -39,4 +52,7 @@ int main()
 	cin>>n;
 	int cg[n];
 	generate_gpa(cg, n);
+	int *B = countingsort1(cg, n);
+	for (int i=0; i<n; i++)
+		cout<<B[i]<<endl;
 }
